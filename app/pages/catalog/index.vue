607da50748a4a7eb365a12b416c2832030df1951
dchakin <dmitrychakin27@gmail.com>
2026-04-06 19:52:13 +0300

    use useFetch

--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -8,12 +8,17 @@ import type { GetCategoriesResponse } from '~/interfaces/category.interface'
 const config = useRuntimeConfig()
 const API_URL = config.public.apiUrl
 
-try {
-  const data = await $fetch<GetCategoriesResponse>(API_URL + '/categories')
-  console.log(data)
-} catch (error) {
-  console.error(error)
-}
+const { data, error, } = await useFetch(API_URL + '/categories')
+
+console.log(data.value);
+console.log(error.value);
+
+// try {
+//   const data = await $fetch<GetCategoriesResponse>(API_URL + '/categories')
+//   console.log(data)
+// } catch (error) {
+//   console.error(error)
+// }
 </script>
 
 <style></style>
