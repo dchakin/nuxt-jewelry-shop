6aab90042f53aacbd23ed66582e4788620ace962
dchakin <dmitrychakin27@gmail.com>
2026-04-06 18:43:08 +0300

    add app transition

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <main class="main">
+      <NuxtPage :transition="{
+        name: 'page',
+        mode: 'out-in'
+      }" />
+    </main>
   </NuxtLayout>
 </template>
 
@@ -9,4 +14,19 @@ import '~/assets/styles/main.css'
 </script>
 
 <style scoped>
+.main {
+  margin: 0 auto;
+  max-width: 1248px;
+  padding: 40px 0;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
