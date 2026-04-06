d3fb1ea12b99bcb9d8e7ef66bbbe88395add0872
dchakin <dmitrychakin27@gmail.com>
2026-04-06 19:38:59 +0300

    use $fetch method

--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    Catalog
-  </div>
+  <div>Catalog</div>
 </template>
 
 <script lang="ts" setup>
+import type { GetCategoriesResponse } from '~/interfaces/category.interface'
 
+const config = useRuntimeConfig()
+const API_URL = config.public.apiUrl
+
+try {
+  const data = await $fetch<GetCategoriesResponse>(API_URL + '/categories')
+  console.log(data)
+} catch (error) {
+  console.error(error)
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
