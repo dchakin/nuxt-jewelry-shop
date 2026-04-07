0802b0b89e145abea90c6b47347027ea45fb9aea
dchakin <dmitrychakin27@gmail.com>
2026-04-07 20:24:50 +0300

    add catalog card

--- app/pages/catalog/index.vue
+++ app/pages/catalog/index.vue
@@ -1,24 +1,77 @@
-<template>
-  <div>Catalog</div>
-</template>
-
-<script lang="ts" setup>
+<script setup lang="ts">
 import type { GetCategoriesResponse } from '~/interfaces/category.interface'
+import type { Product } from '~/interfaces/product.interface'
 
 const config = useRuntimeConfig()
 const API_URL = config.public.apiUrl
+const select = ref('')
 
-const { data, error, } = await useFetch(API_URL + '/categories')
+const { data } = await useFetch<GetCategoriesResponse>(`${API_URL}/categories`)
 
-console.log(data.value);
-console.log(error.value);
+const selectDefault = { value: '', label: 'Категории' }
+const categoriesSelect = computed(() => {
+  return data.value
+    ? data.value?.categories
+        .map((c) => ({
+          value: c.id.toString(),
+          label: c.name,
+        }))
+        .concat(selectDefault)
+    : [selectDefault]
+})
 
-// try {
-//   const data = await $fetch<GetCategoriesResponse>(API_URL + '/categories')
-//   console.log(data)
-// } catch (error) {
-//   console.error(error)
-// }
+
+const product: Product = {
+  id: 1,
+  name: "Lira Earrings",
+  price: 20,
+  short_description: "Элегантные золотистые серьги-кольца",
+  long_description:
+    "Отлично подойдут к любому гардеробу. Чистое золото высокой пробы, которое не оставит вас равнодушными к качеству изделия.",
+  sku: "12",
+  discount: 0,
+  images: [
+    "/images/lira1.jpg",
+    "/images/lira2.jpg",
+    "/images/lira3.jpg",
+    "/images/lira4.jpg",
+  ],
+  category_id: 1,
+  category: {
+    id: 1,
+    name: "Серьги",
+    alias: "earrings",
+  },
+  created_at: new Date(),
+  updated_at: new Date(),
+};
 </script>
 
-<style></style>
+<template>
+  <div>
+    <h1 class="left">Каталог товаров</h1>
+
+    <div class="catalog">
+      <div class="catalog__filter">
+        <SelectField
+          v-model="select"
+          :options="categoriesSelect" />
+      </div>
+
+      <div>
+         <CatalogCard v-bind="product" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.catalog {
+  display: flex;
+  gap: 36px;
+}
+
+.catalog__filter {
+  width: 260px;
+}
+</style>
