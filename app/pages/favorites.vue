cdf17f65d2f2910ae56160739bd08ae05364d38b
dchakin <dmitrychakin27@gmail.com>
2026-04-24 19:19:19 +0300

    add favorites list

--- app/pages/favorites.vue
+++ app/pages/favorites.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>
-    {{ favoritesStore.favoritesIds[0] }}
+  <div class="catalog__grid">
+    <CatalogCard
+      v-for="product in products"
+      :key="product.id"
+      v-bind="product" />
   </div>
 </template>
 
 <script lang="ts" setup>
-const favoritesStore = useFavoritesStore()
+import type { Product } from '~/interfaces/product.interface'
 
-favoritesStore.addToFavorite(1)
+const favoritesStore = useFavoritesStore()
+const config = useRuntimeConfig()
+const API_URL = config.public.apiUrl
+const products = ref<Product[]>()
+
+watchEffect(async () => {
+  const data = await Promise.all(
+    favoritesStore.favoritesIds.map((id) => {
+      return $fetch<{ product: Product }>(`${API_URL}/products/${id}`)
+    }),
+  )
+
+  products.value = data.map((el) => el.product)
+})
 </script>
 
-<style></style>
+<style scoped>
+.catalog__grid {
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 64px 12px;
+}
+</style>
